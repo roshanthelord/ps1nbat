31942cbf2077c40ee246eac3c07f317f538a30ea
roshanthelord <121232265+roshanthelord@users.noreply.github.com>
2025-06-21 08:34:38 +0700

    Add files via upload

---
+++ listing_saved_ssid+key.ps1
@@ -0,0 +1,14 @@
+foreach ($profile in netsh wlan show profiles | Select-String -Pattern "All User Profile" | ForEach-Object {($_ -split ":")[1].Trim()}) {
+    $keyInfo = netsh wlan show profile name=$profile key=clear | Select-String -Pattern "Key Content"
+    if ($keyInfo) {
+        # Extract the Key Content
+        $key = ($keyInfo -split ":")[1].Trim()
+        Write-Host "SSID: $profile"
+        Write-Host "Key: $key"
+        Write-Host "---------------------------------"
+    } else {
+        Write-Host "SSID: $profile"
+        Write-Host "Key: (No key set)"
+        Write-Host "---------------------------------"
+    }
+}
